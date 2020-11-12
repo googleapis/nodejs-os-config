be899930bbd76441ef9ba77fa456f6dd9841d3f4
yoshi-automation <yoshi-automation@google.com>
2020-11-12 04:53:34 -0800

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- system-test/fixtures/sample/src/index.ts
+++ system-test/fixtures/sample/src/index.ts
@@ -18,15 +18,8 @@
 
 import {OsConfigServiceClient} from '@google-cloud/os-config';
 
-// check that the client class type name can be used
-function doStuffWithOsConfigServiceClient(client: OsConfigServiceClient) {
-  client.close();
-}
-
 function main() {
-  // check that the client instance can be created
-  const osConfigServiceClient = new OsConfigServiceClient();
-  doStuffWithOsConfigServiceClient(osConfigServiceClient);
+  new OsConfigServiceClient();
 }
 
 main();
